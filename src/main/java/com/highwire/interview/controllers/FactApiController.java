0e9298a6ce502995dd9ce95a4343f8d92bd1341e
Brajveer Singh <brajveer.singh@faichi.com>
2017-04-19 15:23:06 +0530

    Commented extra Fact creation logic in PostConstruct

--- src/main/java/com/highwire/interview/controllers/FactApiController.java
+++ src/main/java/com/highwire/interview/controllers/FactApiController.java
@@ -35,13 +35,13 @@ public class FactApiController {
     /**
      * Uncomment this code if you want to create some initial data on startup for testing endpoints.
      */
-    @PostConstruct
+   /* @PostConstruct
     public void createInitialEntities() {
         this.factService.createFact(new Fact("The sky is blue", new FactCategory("science")));
         this.factService.createFact(new Fact("Grass is green", new FactCategory("science")));
         this.factService.createFact(new Fact("I think, therefore I am", new FactCategory("philosophy")));
         this.factService.createFact(new Fact("I think, but I am not sure", new FactCategory("computing")));
-    }
+    }*/
     
     
 }
